b54b691fd5b6f9ce0f02598ed652a1ced63e3b29
Atero18 <atero18@hotmail.fr>
2017-03-19 22:37:35 +0100

    Prepared code for a window
    Chose the Pygame library, then prepared a little the changing
    orientation of Pac-Man

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # Main file of our Pac-Man copy
 # Pac-Man Begins : A Unyss creation
 # PCB Version : alpha 0.0.1
-# File version : 1
+# File version : 2
+
+# Pygame is the main graphic library we use
+import pygame
+
+import settings
+
+# Define if Pac-Man or ghosts can go in a direction
+def allowedToMov(x,y ,direction):
+    return True
 
 
 class PacMan:
@@ -9,30 +18,52 @@ class PacMan:
     name = 'Bernard'
     life = 1
     x, y = 0,0
-    orientation = 'R'
+    orientation, wantedOrientation, isMoving = 'R', 'R', False
     godmod = False # Define if PacMan is allowed to fight ghosts
 
     def __init__ (self,name):
         self.name = name
 
-    def movement(self, dep):
-        if(dep=='U'):
-            pass
-        elif(dep=='D'):
-            pass
-        elif(dep=='L'):
-            pass
-        elif(dep=='R'):
-            pass
+    # Handle the user volunteer of changing Pac-Man Orientation
+    def changeOrientation(self, changeMov):
+        if(changeMov!='U' and changeMov!='D' and changeMov!='L'
+          and changeMov!='R'):
+            print("Error #1 : the 'changeMov' parameter seems incorrect.")
+
         else:
-            print("Error #1 : The 'dep' parameter seems incorrect.")
+            wantedOrientation = changeMov
+            # If Pac-Man can mov now in the wanted orientation, we change is
+            # orientation and allow him to move on. If he can't, we record
+            # the 'wanted Direction' in case if he stop is course, he will
+            # rotate in this orientation.
+            if(allowedToMov(self.x, self.y, self.wantedOrientation)):
+                orientation = wantedOrientation
+                isMoving = True
+
+    # Handle the movement of Pac-Man.
+    def movement(self):
+        if(orientation=='U'):
+            pass
+        elif(orientation=='D'):
+            pass
+        elif(orientation=='L'):
+            pass
+        elif(orientation=='R'):
+            pass
+
+        if(not(isMoving)):
+            orientation = wantedOrientation
+
 
     # Return all the PacMan Properties as a list
     def data(self):
         liste = [self.name, self.life, self.x, self.y,
-        self.orientation, self.godmod]
+        self.orientation, self.wantedOrientation, self.isMoving,
+        self.godmod]
         return liste
 
 
 pacman = PacMan('Sheldon')
 print(pacman.data())
+
+windows = pygame.display.set_mode((settings.width, settings.height))
---
+++ settings.py
@@ -0,0 +1,9 @@
+# Here are all the differents settings user has to adjust.
+# Some of them (as width and height) should be adjusted automatically
+# in the future.
+# File Version : 1
+
+
+# Graphics parameters
+width = 400
+height = 250
